158d04ee9967e545c755626ae38d7a86f076139a
Amit Kundu <akundu959@gmail.com>
2022-08-03 00:27:55 -0700

    Initial Commit

---
+++ spring-jdbc/src/main/java/com/coforge/training/spring_jdbc/App.java
@@ -0,0 +1,13 @@
+package com.coforge.training.spring_jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
